ac32877ec8d6976923fd4499c04471692c3ca028
Erik Zenker <erikzenker@posteo.de>
2019-08-01 09:47:21 +0200

    Add simple example

---
+++ example/main.cpp
@@ -0,0 +1,58 @@
+#include "hsm/hsm.h"
+
+#include <cassert>
+
+// States
+struct Locked {
+};
+struct Unlocked {
+};
+
+// Events
+struct Push {
+};
+struct Coin {
+};
+
+// No events nor guards yet
+const auto none = []() {};
+
+struct Turnstile {
+    constexpr auto make_transition_table()
+    {
+        // clang-format off
+        return hsm::transition_table(
+            //              Start      , Event              , Guard   , Action , Target
+            //             +-----------+--------------------+---------+--------+---------------+
+            hsm::transition(Locked {}  , hsm::event<Push> {}, none    , none   , Locked {}    ),
+            hsm::transition(Locked {}  , hsm::event<Coin> {}, none    , none   , Unlocked {}  ),
+            //            +------------+--------------------+---------+--------+---------------+
+            hsm::transition(Unlocked {}, hsm::event<Push> {}, none    , none   , Locked {}    ),
+            hsm::transition(Unlocked {}, hsm::event<Coin> {}, none    , none   , Unlocked {} ));
+            //            +------------+--------------------+-------+----------+---------------+
+        // clang-format on
+    }
+
+    constexpr auto initial_state()
+    {
+        return Locked {};
+    }
+};
+
+int main()
+{
+    hsm::Sm<Turnstile> turnstileSm;
+
+    // The turnstile is initially locked
+    assert(turnstileSm.is(Locked {}));
+
+    // Inserting a coin unlocks it
+    turnstileSm.process_event(Coin {});
+    assert(turnstileSm.is(Unlocked {}));
+
+    // Entering the turnstile will lock it again
+    turnstileSm.process_event(Push {});
+    assert(turnstileSm.is(Locked {}));
+
+    return 0;
+}
