6d03e51e2855dfc2553875b53ed178b0c630822e
Erik Zenker <erikzenker@posteo.de>
2019-09-02 21:27:23 +0200

    Add simple benchmark from sml project

---
+++ benchmark/simple/hsm.cpp
@@ -0,0 +1,92 @@
+//
+// Copyright (c) 2016-2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "benchmark.hpp"
+#include <hsm/hsm.h>
+    
+using namespace hsm;
+
+struct Empty{};
+struct Open{};
+struct Stopped{};
+struct Playing{};
+struct Pause{};
+
+struct play {};
+struct end_pause {};
+struct stop {};
+struct pause {};
+struct open_close {};
+struct cd_detected {};
+
+auto start_playback = [](auto) {};
+auto resume_playback = [](auto) {};
+auto close_drawer = [](auto) {};
+auto open_drawer = [](auto) {};
+auto stop_and_open = [](auto) {};
+auto stopped_again = [](auto) {};
+auto store_cd_info = [](auto) {};
+auto pause_playback = [](auto) {};
+auto stop_playback = [](auto) {};
+auto noguard = [](auto){ return true;};
+
+struct player {
+  constexpr auto make_transition_table(){
+
+    // clang-format off
+    return transition_table(
+        row( Stopped{}, event<play>{}       , noguard,  start_playback,   Playing{}),
+        row( Pause{},   event<end_pause>{}  , noguard,  resume_playback,  Playing{}),
+        row( Open{},    event<open_close>{} , noguard,  close_drawer,     Empty{}),
+        row( Empty{},   event<open_close>{} , noguard,  open_drawer,      Open{}),
+        row( Pause{},   event<open_close>{} , noguard,  stop_and_open,    Open{}),
+        row( Stopped{}, event<open_close>{} , noguard,  open_drawer,      Open{}),
+        row( Playing{}, event<open_close>{} , noguard,  stop_and_open,    Open{}),
+        row( Playing{}, event<pause>{}      , noguard,  pause_playback,   Pause{}),
+        row( Playing{}, event<stop>{}       , noguard,  stop_playback,    Stopped{}),
+        row( Pause{},   event<stop>{}       , noguard,  stop_playback,    Stopped{}),
+        row( Empty{},   event<cd_detected>{}, noguard,  store_cd_info,    Stopped{}),
+        row( Stopped{}, event<stop>{}       , noguard,  stopped_again,    Stopped{})
+    );
+    // clang-format on
+  }
+
+  auto constexpr initial_state(){
+    return Empty{};  
+  }
+
+};
+
+int main() {
+  hsm::Sm<player> sm;
+
+  auto a = open_close{};  
+  auto b = cd_detected{};
+  auto c = play{};
+  auto d = pause{};
+  auto e = stop{};
+  auto f = end_pause{};
+
+  benchmark_execution_speed([&] {
+    for (auto i = 0; i < 1'000'000; ++i) {
+      sm.process_event(a);
+      sm.process_event(a);
+      sm.process_event(b);
+      sm.process_event(c);
+      sm.process_event(d);
+      // go back to Playing
+      sm.process_event(f);
+      sm.process_event(d);
+      sm.process_event(e);
+      // event leading to the same state
+      sm.process_event(e);
+      sm.process_event(a);
+      sm.process_event(a);
+    }
+  });
+  benchmark_memory_usage(sm);
+}
---
+++ benchmark/simple/sml.cpp
@@ -0,0 +1,79 @@
+//
+// Copyright (c) 2016-2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "benchmark.hpp"
+#include <boost/sml.hpp>
+
+namespace sml = boost::sml;
+
+struct play {};
+struct end_pause {};
+struct stop {};
+struct pause {};
+struct open_close {};
+struct cd_detected {};
+
+auto start_playback = [] {};
+auto resume_playback = [] {};
+auto close_drawer = [] {};
+auto open_drawer = [] {};
+auto stop_and_open = [] {};
+auto stopped_again = [] {};
+auto store_cd_info = [] {};
+auto pause_playback = [] {};
+auto stop_playback = [] {};
+
+struct player {
+  auto operator()() const noexcept {
+    using namespace sml;
+    auto Empty = state<class Empty>;
+    auto Open = state<class Open>;
+    auto Stopped = state<class Stopped>;
+    auto Playing = state<class Playing>;
+    auto Pause = state<class Pause>;
+
+    // clang-format off
+    return make_transition_table(
+        Playing <= Stopped + event<play> / start_playback,
+        Playing <= Pause + event<end_pause> / resume_playback,
+        Empty <= Open + event<open_close> / close_drawer,
+        Open <= *Empty + event<open_close> / open_drawer,
+        Open <= Pause + event<open_close> / stop_and_open,
+        Open <= Stopped + event<open_close> / open_drawer,
+        Open <= Playing + event<open_close> / stop_and_open,
+        Pause <= Playing + event<pause> / pause_playback,
+        Stopped <= Playing + event<stop> / stop_playback,
+        Stopped <= Pause + event<stop> / stop_playback,
+        Stopped <= Empty + event<cd_detected> / store_cd_info,
+        Stopped <= Stopped + event<stop> / stopped_again
+    );
+    // clang-format on
+  }
+};
+
+int main() {
+  sml::sm<player> sm;
+
+  benchmark_execution_speed([&] {
+    for (auto i = 0; i < 1'000'000; ++i) {
+      sm.process_event(open_close{});
+      sm.process_event(open_close{});
+      sm.process_event(cd_detected{});
+      sm.process_event(play{});
+      sm.process_event(pause{});
+      // go back to Playing
+      sm.process_event(end_pause{});
+      sm.process_event(pause{});
+      sm.process_event(stop{});
+      // event leading to the same state
+      sm.process_event(stop{});
+      sm.process_event(open_close{});
+      sm.process_event(open_close{});
+    }
+  });
+  benchmark_memory_usage(sm);
+}
