c11ecf73d07ae6288c9dafb001ad24cba4f9c79f
friendlyanon <friendlyanon@users.noreply.github.com>
2021-01-28 00:10:05 +0000

    Parameterize the build type

--- script/install_gtest.sh
+++ script/install_gtest.sh
@@ -12,12 +12,14 @@ cd build/dependencies
 git clone https://github.com/google/googletest
 (cd googletest; git checkout 6c58c11d5497b6ee1df3cb400ce30deb72fc28c0)
 
+BUILD_TYPE="${1:-Release}"
+
 cmake -S googletest -B googletest/build \
     -D "CMAKE_INSTALL_PREFIX=${PWD}/gtest" \
-    -D CMAKE_BUILD_TYPE=Release
+    -D "CMAKE_BUILD_TYPE=${BUILD_TYPE}"
 
-cmake --build googletest/build
+cmake --build googletest/build --config "${BUILD_TYPE}"
 
-cmake --install googletest/build
+cmake --install googletest/build --config "${BUILD_TYPE}"
 
 rm -rf googletest
