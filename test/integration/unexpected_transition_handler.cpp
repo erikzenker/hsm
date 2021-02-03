1afda17ad85e86cc72c50252eff0e2e39c062def
Erik Zenker <erikzenker@posteo.de>
2021-02-03 14:30:40 +0100

    reproduce: Unexpeced event on failing guard

--- test/integration/unexpected_transition_handler.cpp
+++ test/integration/unexpected_transition_handler.cpp
@@ -13,6 +13,8 @@ struct S1 {
 };
 struct S2 {
 };
+struct S3 {
+};
 
 // Events
 struct e1 {
@@ -25,6 +27,12 @@ struct e2 {
 
     std::shared_ptr<std::promise<void>> called;
 };
+struct e3 {
+    std::shared_ptr<std::promise<void>> called;
+};
+
+// Guards
+constexpr auto alwaysFalse = [](auto...) { return false; };
 
 using namespace ::testing;
 using namespace boost::hana;
@@ -34,8 +42,10 @@ struct MainState {
     {
         // clang-format off
         return hsm::transition_table(
-            * hsm::state<S1> + hsm::event<e1> = hsm::state<S2>,
-              hsm::state<S2> + hsm::event<e2> = hsm::state<S1>
+            * hsm::state<S1> + hsm::event<e1>              = hsm::state<S2>
+            , hsm::state<S1> + hsm::event<e3>              = hsm::state<S3> 
+            , hsm::state<S2> + hsm::event<e2>              = hsm::state<S1>
+            , hsm::state<S3> + hsm::event<e2> [alwaysFalse]= hsm::state<S1>
         );
         // clang-format on
     }
@@ -51,6 +61,7 @@ struct MainState {
 class NoTransitionHandlerTests : public Test {
   protected:
     hsm::sm<MainState> sm;
+    hsm::sm<MainState> sm2;
 };
 
 TEST_F(NoTransitionHandlerTests, should_call_no_transition_handler)
@@ -59,4 +70,13 @@ TEST_F(NoTransitionHandlerTests, should_call_no_transition_handler)
     sm.process_event(e2 { called });
 
     ASSERT_EQ(std::future_status::ready, called->get_future().wait_for(std::chrono::seconds(1)));
+}
+
+TEST_F(NoTransitionHandlerTests, should_not_call_transition_handler_when_guard_fails)
+{
+    auto called = std::make_shared<std::promise<void>>();
+    sm.process_event(e3 {});
+    sm.process_event(e2 { called });
+
+    ASSERT_EQ(std::future_status::timeout, called->get_future().wait_for(std::chrono::seconds(1)));
 }
